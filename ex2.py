983d28c6495870bc652272b6fc6a14e62ab536fd
Timur Darii <timurdarii95@gmail.com>
2026-02-08 15:48:43 +0000

    Finish 1st lists exercise

--- ex2.py
+++ ex2.py
@@ -1,3 +1,10 @@
 statul_1 = 'Franta'
 statul_2 = 'Italia'
 statul_3 = 'Belgia'
+
+state_ale_lumii = ['Franta', 'Italia', 'Belgia']
+
+state_noi = ['Spania', 'Germania']
+state_ale_lumii.extend(state_noi)
+
+print(state_ale_lumii)
