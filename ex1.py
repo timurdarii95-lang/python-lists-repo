4a98d7cf7d88513393c286827b64a3aab6be53eb
Timur Darii <timurdarii95@gmail.com>
2026-02-08 15:32:01 +0000

    initial commit

---
+++ ex1.py
@@ -0,0 +1,9 @@
+lista = [5, 3, 1]
+print(lista)
+
+lista [0] = 50
+print(lista)
+
+
+for list in lista:
+    print(list)
---
+++ ex2.py
@@ -0,0 +1,3 @@
+statul_1 = 'Franta'
+statul_2 = 'Italia'
+statul_3 = 'Belgia'
